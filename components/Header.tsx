2d430a09c03f8ba0f372f9fcec6becf967e58a06
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2022-11-10 03:44:46 +0530

    Header

---
+++ components/Header.tsx
@@ -0,0 +1,80 @@
+import { motion } from "framer-motion";
+import { SocialIcon } from "react-social-icons";
+
+function Header() {
+  return (
+    <header className="sticky top-0 flex justify-between items-start max-w-7xl mx-auto xl:items-center z-20">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
+        <SocialIcon
+          url="https://linkedin.com/in/iabu94"
+          target="_blank"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://iabu94.medium.com"
+          target="_blank"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://github.com/iabu94"
+          target="_blank"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://twitter.com/iabu94"
+          target="_blank"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://facebook.com/iabu94"
+          target="_blank"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div>
+
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center cursor-pointer pr-2"
+      >
+        <SocialIcon network="email" fgColor="gray" bgColor="transparent" />
+        <p className="hidden md:inline-flex uppercase text-gray-400 text-sm">
+          Get in Touch
+        </p>
+      </motion.div>
+    </header>
+  );
+}
+
+export default Header;
